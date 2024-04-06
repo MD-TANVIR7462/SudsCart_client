7ce22d44994db9631136ffb9a325ee546aa2f1ce
unknown <mdtanvir7462@gmail.com>
2024-04-06 18:44:14 +0600

    manualy install tailwind css and fixed the bug of styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,11 @@ import {nextui} from "@nextui-org/react";
 
 const config: Config = {
   content: [
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
